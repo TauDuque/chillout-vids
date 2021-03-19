e2e2402fc2c4092bdef0b6b02f737c537c5a46ef
Tau <hectauduque@gmail.com>
2021-03-19 08:16:46 -0300

    oh my were back again

--- script.js
+++ script.js
@@ -1,131 +1,163 @@
-const image = document.querySelector("img");
-const music = document.querySelector("audio");
-const progressContainer = document.getElementById("progress-container");
-const progress = document.getElementById("progress");
-const currentTimeEl = document.getElementById("current-time");
-const durationEl = document.getElementById("duration");
-const title = document.getElementById("title");
-const prevBtn = document.getElementById("prev");
-const playBtn = document.getElementById("play");
-const nextBtn = document.getElementById("next");
+const player = document.querySelector(".player");
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const prorgessBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullScreenBtn = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
-//musicas
-const musicas = [
-  {
-    name: "a-agua-e-uma-so",
-    displayName: "A Água é Uma Só",
-  },
-  {
-    name: "capital-do-nordeste",
-    displayName: "Capital do Nordeste",
-  },
-  {
-    name: "fundacao-sem-fundamento",
-    displayName: "Fundação Sem Fundamento",
-  },
-  {
-    name: "tarabando",
-    displayName: "Tarabando",
-  },
-  {
-    name: "xote-dos-recursos",
-    displayName: "Xote dos Recursos",
-  },
-];
+// Play & Pause ----------------------------------- //
 
-// verificar se toca
-let taTocando = false;
-
-//play
-function tocarMusica() {
-  taTocando = true;
-  playBtn.classList.replace("fa-play-circle", "fa-pause-circle");
-  music.play();
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "play");
 }
 
-//pausar
-function pausarMusica() {
-  taTocando = false;
-  playBtn.classList.replace("fa-pause-circle", "fa-play-circle");
-  music.pause();
-}
-
-// tocar ou pausar event listener
-playBtn.addEventListener("click", () =>
-  taTocando ? pausarMusica() : tocarMusica()
-);
-
-// atualizar DOM
-function carregarMusica(musicas) {
-  title.textContent = musicas.displayName;
-  music.src = `music/${musicas.name}.mp3`;
-  image.src = `img/${musicas.name}.jpg`;
-}
-
-// musica atual
-let songIndex = 0;
-
-//ultima musica
-function musicaAntes() {
-  songIndex--;
-  if (songIndex < 0) {
-    songIndex = musicas.length - 1;
-  }
-  carregarMusica(musicas[songIndex]);
-  tocarMusica();
-}
-
-//proxima musica
-function musicaDepois() {
-  songIndex++;
-  if (songIndex > musicas.length - 1) {
-    songIndex = 0;
-  }
-  carregarMusica(musicas[songIndex]);
-  tocarMusica();
-}
-
-// On Load - selecionar musica
-carregarMusica(musicas[songIndex]);
-
-// atualizar barra de progresso e tempo
-function atualizarProgresso(e) {
-  if (taTocando) {
-    const { duration, currentTime } = e.srcElement;
-    // atualizar barra de progresso
-    const percentualProgresso = (currentTime / duration) * 100;
-    progress.style.width = `${percentualProgresso}%`;
-    //calcular duração das musicas
-    const minutosDur = Math.floor(duration / 60);
-    let segundosDur = Math.floor(duration % 60);
-    if (segundosDur < 10) {
-      segundosDur = `0${segundosDur}`;
-    }
-    //atrasar troca da duração p/ evitar NaN
-    if (segundosDur) {
-      durationEl.textContent = `${minutosDur}:${segundosDur}`;
-    }
-    //calcular tempo atual das musicas
-    const minutosAtual = Math.floor(currentTime / 60);
-    let segundosAtual = Math.floor(currentTime % 60);
-    if (segundosAtual < 10) {
-      segundosAtual = `0${segundosAtual}`;
-    }
-    currentTimeEl.textContent = `${minutosAtual}:${segundosAtual}`;
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "pause");
+  } else {
+    video.pause();
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "play");
   }
 }
 
-//definir barra de progresso
-function barraProgressiva(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const { duration } = music;
-  music.currentTime = (clickX / width) * duration;
+// No fim do vídeo, mostrar play btn
+video.addEventListener("ended", showPlayIcon);
+
+// Progress Bar ---------------------------------- //
+
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
 }
 
-// event listeners
-prevBtn.addEventListener("click", musicaAntes);
-nextBtn.addEventListener("click", musicaDepois);
-music.addEventListener("ended", musicaDepois);
-music.addEventListener("timeupdate", atualizarProgresso);
-progressContainer.addEventListener("click", barraProgressiva);
+//atualizar a barra de progresso em tempo real
+function updateProgress() {
+  prorgessBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)}/`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// selecionar momentos do vídeo
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  prorgessBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+  console.log(newTime);
+}
+
+// Volume Controls --------------------------- //
+
+let lastVolume = 1;
+
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // arredondando o volume
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+
+  // mudar o ícone do volume
+  volumeIcon.className = "";
+  if (volume > 0.7) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+  lastVolume = volume;
+}
+
+// mutar
+
+function toggleMute() {
+  volumeIcon.className = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Desmutar");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "mutar");
+  }
+}
+
+// Change Playback Speed -------------------- //
+
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
+
+// Fullscreen ------------------------------- //
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  video.classList.add("video-fullscreen");
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+  video.classList.remove("video-fullscreen");
+}
+
+let fullscreen = false;
+
+// ativar fullscreen
+function toggleFullScreen() {
+  if (!fullscreen) {
+    openFullscreen(player);
+  } else {
+    closeFullscreen();
+  }
+  fullscreen = !fullscreen;
+}
+
+// Event listeners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
+fullScreenBtn.addEventListener("click", toggleFullScreen);
